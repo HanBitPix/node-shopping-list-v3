e094f7be69b709555e08939785bf23d4370e33c4
HanBitPix <hanjt517@gmail.com>
2018-03-29 17:53:52 -0700

    Created Delete Endpoints for Recipes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const express = require('express');
 const router = express.Router();
@@ -54,6 +55,10 @@ app.delete('/shopping-list/:id', (req, res) => {
 });
 
 
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+});
+
 // when new recipe added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
 // if okay, add new item, and return it with a status 201.
@@ -72,10 +77,11 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
-})
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.id}\``);
+  res.status(204).end();
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
